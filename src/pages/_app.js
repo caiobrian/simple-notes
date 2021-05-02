5231c02f7afc219a7ce5518c8f3c4f4bd4324373
caiobrian <caiogomeslog@gmail.com>
2021-05-01 22:01:52 -0300

    refactor: change file order and components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/global'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Simple Notes</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cutive&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="#06092B" />
+        <meta name="description" content="Simple Notes" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,5 @@
-import Head from 'next/head'
-import Button from '../components/Button'
-import Divider from '../components/Divider'
-import PostBody from '../components/PostBody'
-import PostTitle from '../components/PostTitle'
-import WordCounter from '../components/WordCounter'
-import { StateProvider } from '../contexts/state'
-import { initialTextState, textReducer } from '../reducer/textReducer'
-import styles from '../styles/Home.module.css'
+import Main from './_main'
 
 export default function Home() {
-  return (
-    <StateProvider initialState={initialTextState} reducer={textReducer}>
-      <div className={styles.container}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cutive&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-
-        <main className={styles.main}>
-          <PostTitle />
-          <Divider />
-          <PostBody />
-        </main>
-        <WordCounter />
-
-        <div className={styles.buttonContainer}>
-          <Button>Salvar</Button>
-        </div>
-      </div>
-    </StateProvider>
-  )
+  return <Main />
 }
