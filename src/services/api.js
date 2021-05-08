6e439f8246a0949d145d18cf2f7d6b2245c39b1b
caiobrian <caiogomeslog@gmail.com>
2021-05-07 22:35:27 -0300

    update database config

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const endpoint = 'http://localhost:1337'
+const endpoint =
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV === 'production'
+    ? 'https://simple-notes-db-01.herokuapp.com'
+    : 'http://localhost:1337'
 
 export const api = axios.create({
   baseURL: endpoint,
