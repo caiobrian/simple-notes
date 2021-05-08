97562c11c3776d1e0bd69bea2a828ccf23cfc28a
caiobrian <caiogomeslog@gmail.com>
2021-05-07 21:58:15 -0300

    refactor: change preview notes container margin bottom

--- src/components/AllNotesPreview/styles.js
+++ src/components/AllNotesPreview/styles.js
@@ -5,12 +5,12 @@ export const Container = styled.div`
   width: 90%;
   display: flex;
   border-radius: 4px;
-  margin-bottom: ${theme.spacings.xlarge};
+  margin-bottom: ${theme.spacings.small};
   padding: ${theme.spacings.small};
   overflow-x: auto;
 
   :-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.8);
     border-radius: 10px;
     background-color: #f5f5f5;
   }
@@ -22,7 +22,7 @@ export const Container = styled.div`
 
   ::-webkit-scrollbar-thumb {
     border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.8);
     background-color: #555;
   }
 `
